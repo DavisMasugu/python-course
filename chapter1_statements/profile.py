d1b3c0518e92dc8a72976bdf01c6e5b549b19e9c
Davis Masugu <davismasugu2020@gmail.com>
2026-05-10 06:34:25 +0300

    Improved Day 2 profile program formatting

---
+++ chapter1_statements/profile.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+
+print(f"Hello {name}")
+print(f"Hello{name},Welcome to python")
+print(f"Hello {name}, welcome to Python programming")
+print(f"Your name has {len(name)} letters")
