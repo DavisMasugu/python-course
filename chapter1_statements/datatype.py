96a45de90d2963af7070928fb02133ae29065e37
Davis Masugu <davismasugu2020@gmail.com>
2026-05-11 06:11:11 +0300

    Day 3: learned python data types

---
+++ chapter1_statements/datatype.py
@@ -0,0 +1,16 @@
+# Python data types
+
+student_name = "Davis"
+student_age = 25
+student_weight = 65.5
+is_present = True
+
+print(student_name)
+print(student_age)
+print(student_weight)
+print(is_present)
+
+print(type(student_name))
+print(type(student_age))
+print(type(student_weight))
+print(type(is_present))
